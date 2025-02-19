a9aafc031a596803457c9fab64710484b81d245c
alex <rusanov-ao@inbox.ru>
2025-02-19 21:46:24 +0300

    add pattern composite

---
+++ src/main/java/org/example/composite/Car.java
@@ -0,0 +1,12 @@
+package org.example.composite;
+/**
+ * Интерфейс Car определяет общий метод draw(String color),
+ * который должен реализовывать любой объект, представляющий автомобиль.
+ */
+
+public interface Car {
+
+    void draw(String color);
+}
+
+
---
+++ src/main/java/org/example/composite/Draw.java
@@ -0,0 +1,34 @@
+package org.example.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс Drawing является композитом (или компоновщиком). Он:
+ * Хранит список объектов типа Car.
+ * Реализует метод draw(String color) так, чтобы он вызывал этот метод для каждого объекта в списке.
+ * Предоставляет методы add(Car car) для добавления новых автомобилей и clear() для очистки списка.
+ * Таким образом, Drawing может содержать как листья (SportCar, UnknownCar), так и другие композиты
+ * (если бы они существовали).
+ */
+
+public class Draw implements Car {
+
+    private List<Car> cars = new ArrayList<>();
+
+    @Override
+    public void draw(String color) {
+        for (Car car : cars) {
+            car.draw(color);
+        }
+    }
+
+    public void add(Car car) {
+        this.cars.add(car);
+    }
+
+    public void clear() {
+        System.out.println();
+        this.cars.clear();
+    }
+}
