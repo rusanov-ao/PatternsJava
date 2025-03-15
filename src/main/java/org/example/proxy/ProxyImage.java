fb932fc560b5950e6921609f0923717cbdd5203e
alex <rusanov-ao@inbox.ru>
2025-03-15 22:21:36 +0300

    add pattern proxy

---
+++ src/main/java/org/example/proxy/ProxyImage.java
@@ -0,0 +1,19 @@
+package org.example.proxy;
+
+public class ProxyImage implements Image{
+    private String file;
+    private RealImage image;
+
+    public ProxyImage(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public void display() {
+        if (image == null) {
+            image = new RealImage(file);
+        }
+
+        image.display();
+    }
+}
---
+++ src/main/java/org/example/proxy/RealImage.java
@@ -0,0 +1,20 @@
+package org.example.proxy;
+
+public class RealImage implements Image{
+
+    private String file;
+
+    public RealImage(String file) {
+        this.file = file;
+        load(file);
+    }
+
+    private void load(String file) {
+        System.out.println("Загрузка " + file);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Просмотр " + file);
+    }
+}
