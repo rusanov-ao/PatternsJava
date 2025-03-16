2ebd13a176706596d5cf2aef334ee57b337127cf
alex <rusanov-ao@inbox.ru>
2025-03-16 14:31:17 +0300

    add pattern flyWeight

---
+++ src/main/java/org/example/flyweight/Car.java
@@ -0,0 +1,20 @@
+package org.example.flyweight;
+
+// Context (Контекст)
+public class Car {
+
+    private final CarModel model;
+    private final String color;
+    private final int year;
+
+    public Car(CarModel model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public void displayFullInfo() {
+        model.displayInfo();
+        System.out.println("Цвет: " + color + ", Год выпуска: " + year);
+    }
+}
