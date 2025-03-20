f7ed979f0a0b4e30145fd34cbbb890802a11474c
alex <rusanov-ao@inbox.ru>
2025-03-20 23:08:54 +0300

    add pattern mediator

--- src/main/java/org/example/behavioral/mediator/Mediator.java
+++ src/main/java/org/example/behavioral/mediator/Mediator.java
@@ -1,5 +1,6 @@
 package org.example.behavioral.mediator;
 
+// Пользователи будут использовать метод у Медиатора для общения
 public class Mediator {
 
     public static void sendMessage(User user, String msg) {
